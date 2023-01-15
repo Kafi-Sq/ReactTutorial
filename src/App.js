d84564f49b141a47d17b266dbb1fd009328e0950
Hassen Hassen <xasansq@gmail.com>
2023-01-15 09:38:48 -0600

    Where

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/layout/Layout';
+
+import AllMeetupsPage from './pages/AllMeetup';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetup';
+
 function App() {
-  return <div></div>;
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<AllMeetupsPage />} />
+                <Route path="/new-meetup" element={<NewMeetupsPage />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,5 @@
+function NewMeetupsPage(){
+    return <div>New Meetups page</div>
+}
+
+export default NewMeetupsPage;
